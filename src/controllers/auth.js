530c84c652ed5178458f928bf4ce737f7170b62b
Pham Hong Son <sontrtb@gmail.com>
2023-05-01 21:35:19 +0700

    validate

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,9 +3,9 @@ const auth = require("../services/auth")
 const register = async (req, res) => {
     try {
         const {userName, password, name} = req.body;
-        if(!userName || ! password || !name) return res.status(400).json({
+        if(!userName || ! password || !name || userName?.length ===0 || password?.length ===0 || name?.length ===0) return res.status(400).json({
             errCode: 1,
-            mess: "Tên, Tài khoản hoặc mật khẩu không hợp lệ"
+            mess: "Tên, Tài khoản hoặc mật khẩu không được để trống"
         })
     
         const response = await auth.register(req.body)
